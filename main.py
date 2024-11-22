f8353b0dbea4018947f71f4f4a73e8a5d8145c72
BLR <bruff@Macky-Max.local>
2024-11-22 01:09:28 -0500

    this function calculates the area of a triangle

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ imports math so that the program can access mathematical functions
 
 def area_of_rectangle(length, width):
     return length * width
-
-"""Multiplies the two numbers and returns the result
-
+"""Multiplies the length and the width of a rectangle and returns the result
 Args:
     length: the first sumand.
     width: the second summand.
-    
 Returns:
-    The result of the two numbers.
+    The result of the length and the width(area of rectangle).
+"""
+
+def area_of_triangle(base, height):
+    return 0.5 * base * height
+"""Multiplies 0.5 times the base times the height and returns the result
+Args:
+    0.5: decimal
+    base: the first summand.
+    height: the second summand.
+Returns:
+    The result of 0.5 * base * height(area of triangle).
 """
