5ade8a0630c4ecb96392109eaa20e96d87b07a6f
BLR <bruff@LAPTOP-TM2P1JOK.vsu.edu>
2024-11-22 15:41:39 -0500

    Created the function the allows user enter measurments based off what shape they choosed and edited the directions function

--- main.py
+++ main.py
@@ -35,16 +35,48 @@ Returns:
     The result of radius times 2 (area of circle).
 """
 
-def main():
-    print("Area Calculator!")
-    print("Choose a shape and calculate the area:")
-    print("1.) Rectangle")
-    print("2.) Triangle")
-    print("3.) Circle") 
+
+print("Area Calculator!")
+print("Choose a shape and calculate the area(in cm):")
+print("1.) Rectangle")
+print("2.) Triangle")
+print("3.) Circle") 
     
-    choice = input("Enter your choice 1,2,3")
-    
+choice = input("Enter your choice 1,2,3: ") 
+   
 """Menu displayed and ask input from user
 Returns:
     Displays the title and ask the user what shape they want to calculate as an input.
+"""
+
+
+
+
+if choice == '1':
+        length = float(input("Enter the length of the rectangle: "))
+        width = float(input("Enter the width of the rectangle: "))
+        area = area_of_rectangle(length, width)
+        print(f"The area of the rectangle is: {area} cm")
+        
+elif choice == '2':
+        base = float(input("Enter the base of the triangle: "))
+        height = float(input("Enter the height of the triangle: "))
+        area = area_of_triangle(base, height)
+        print(f"The area of the triangle is: {area} cm")
+        
+elif choice == '3':
+        radius = float(input("Enter the radius of the circle: "))
+        area = area_of_circle(radius)
+        print(f"The area of the circle is: {area} cm")
+        
+else:
+        print("That is not an option PLEASE TRY AGAIN! Please enter 1, 2, or 3.")
+
+"""Multiplies the radius times 2 and returns the result
+Args:
+    User enters the number they wanted calculated.
+    
+Returns:
+    The result of the area calculation of the shpae they choosed.
+    Or Invalid response.
 """
