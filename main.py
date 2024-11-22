913ba2207b9d84944cd49bd8ba3573870cdda4a6
BLR <bruff@Macky-Max.local>
2024-11-22 00:44:22 -0500

    this function calculates the area of rectangle

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import math
+
 """
 imports math so that the program can access mathematical functions
+"""
+
+def area_of_rectangle(length, width):
+    return length * width
+
+"""Multiplies the two numbers and returns the result
+
+Args:
+    length: the first sumand.
+    width: the second summand.
+    
+Returns:
+    The result of the two numbers.
 """
